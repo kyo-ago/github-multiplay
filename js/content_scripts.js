f74b2f63fe1b8209c8af59db45c93d7d85b60b75
kyo_ago <kyo@0-9.sakura.ne.jp>
2013-07-29 22:39:40 +0900

    add gh-pages

--- js/content_scripts.js
+++
@@ -1,6 +0,0 @@
-(function () {
-    var scp = document.createElement('script');
-    scp.src = (chrome.extension).getURL('/web_accessible_resources/inner.js');
-    document.body.appendChild(scp);
-})();
-//@ sourceMappingURL=content_scripts.js.map
